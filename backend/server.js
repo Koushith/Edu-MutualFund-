29cc6007886f37c67468683a45edc1aadc599965
koushith <koushith97@gmail.com>
2021-07-26 23:31:49 +0530

    make it better

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
@@ -14,15 +15,27 @@ connectDB();
 app.use(cors());
 // middleware to read the form data
 app.use(express.json());
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
+// app.get('/', (req, res) => {
+//   res.send('API is running');
+// });
 
 // all routes
 app.use('/api/users', userRoutes);
 
 // get all funds
 app.use('/api/funds', fundRoutes);
+
+// deployment
+const __dirname = path.resolve();
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')));
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 // path not found- middleware
 app.use(notFound);
